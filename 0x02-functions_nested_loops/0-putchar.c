ad1de4049241a6ffaee15ed075371a08c699c70c
Ebez10 <ebereonohebez@gmail.com>
2023-02-22 11:51:43 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: Program that prints _putchar
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char x[] = "_putchar";
+	int i;
+
+	for (i = 0; x[i]; i++)
+	_putchar(x[i]);
+
+	_putchar('\n');
+
+	return 0;
+}
+
