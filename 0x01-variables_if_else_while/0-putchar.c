4c695673915527a4c5d9d7566affcb539cb34390
Ebez10 <ebereonohebez@gmail.com>
2023-02-21 23:48:05 -0800

    _putchar

---
+++ 0x01-variables_if_else_while/0-putchar.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+/**
+ * main - Prints _putchar, followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	char str[] = "_putchar\n";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		char c = str[i];
+
+		-putchar(c);
+		i++;
+	}
+	return (0);
+}
