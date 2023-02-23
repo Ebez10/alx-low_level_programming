70d67b15803e9772042241ea438eb8a19f73a144
Ebez10 <ebereonohebez@gmail.com>
2023-02-22 18:05:43 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,22 +2,31 @@
 /**
  * jack_bauer - prints each minute of the day
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 void jack_bauer(void)
-{
-	int min, hour;
 
-	for (hour = o; hour <= 23; hour++)
+{
+	int a, b, c, d;
+
+	for (a = 48; a <= 50; a++)
 	{
-		for (min = 0; min <= 59; min++)
+		for (b = 48; b <= 57; b++)
 		{
-			_putchar((hour / 10) + '0');
-			_putchar((hour & 10) + '0');
-			_putchar(':');
-			_putchar((min / 10) + '0');
-			_putchar((min % 10) + '0');
-			_putchar(10)
+			for (c = 48; c <= 53; c++)
+			{
+				for (d = 48; d <= 57; d++)
+				{
+					if (a >= 50 && b >= 53)
+						break;
+					_putchar(a);
+					_putchar(b);
+					_putchar(58);
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
+			}
 		}
 	}
 }
