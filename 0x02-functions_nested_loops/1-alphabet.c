517af9b555b5e0bc17d29f7daa7709e946bb8396
Ebez10 <ebereonohebez@gmail.com>
2023-02-22 16:16:39 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: Print alphabets
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
